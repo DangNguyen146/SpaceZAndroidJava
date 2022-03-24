2670cf5dea9f70c74bafbf23f813e2f3d72387e0
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-03-25 00:51:32 +0700

    biding

--- app/src/main/java/com/example/spacezandroidjava/DetailProductActivity.java
+++ app/src/main/java/com/example/spacezandroidjava/DetailProductActivity.java
@@ -2,7 +2,23 @@ package com.example.spacezandroidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.spacezandroidjava.Model.Product;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import lombok.val;
 
 public class DetailProductActivity extends AppCompatActivity {
 
@@ -10,5 +26,44 @@ public class DetailProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_product);
+        Intent intent=getIntent();
+        TextView name_tv=(TextView) findViewById(R.id.product_name);
+    TextView price_tv=(TextView) findViewById(R.id.product_price);
+        TextView description_tv=(TextView) findViewById(R.id.product_description);
+        RatingBar rate_bar=(RatingBar) findViewById(R.id.rating_btn);
+        Button btn_cart=(Button) findViewById(R.id.btn_add_to_cart);
+        btn_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               CharSequence[] items={"1","2","3"};
+                MaterialAlertDialogBuilder builder=new MaterialAlertDialogBuilder(DetailProductActivity.this);
+                builder.setTitle("Chọn nội dung hiển thị trên thẻ")
+                        .setSingleChoiceItems(items, 0,new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Log.i("khfaskj", "onClick: ");
+                            }
+                        });
+
+
+                builder.show();
+            }
+        });
+        if(intent.getExtras()!=null){
+            Product p=(Product) intent.getSerializableExtra("product");
+            String name= p.getName();
+            Long price=p.getPrice();
+            int rate=p.getRate();
+            rate_bar.setRating(rate);
+            String description=p.getDescription();
+            name_tv.setText(name);
+
+            price_tv.setText("Giá: " +price.toString()+" VND");
+            description_tv.setText(description);
+
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/spacezandroidjava/Model/Product.java
+++ app/src/main/java/com/example/spacezandroidjava/Model/Product.java
@@ -1,5 +1,7 @@
 package com.example.spacezandroidjava.Model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 import lombok.Getter;
@@ -10,11 +12,17 @@ import lombok.Setter;
 public class Product  implements Serializable {
 
     private  int id;
+    @SerializedName("description")
+    private String  description;
     private String name;
     private  String image;
     private  String imageSau;
     private String locatedText;
-    private int categoryId;
-    private int idTypes;
+
+    private Long price;
+    private Long view;
+    private int rate;
+    private Object category;
+    private Object tag;
 
 }
